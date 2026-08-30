40b583af1280e8c5b47c5d150f77de22a9a9f860
papi zen <zentot015@gmail.com>
2026-08-30 18:20:18 +0700

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// File: tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
